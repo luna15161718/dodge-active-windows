65140e7f0eae77ae1199ed5475f3a109037d7b22
luna15161718 <lunakenny345@gmail.com>
2026-07-14 00:44:40 +0100

    Added window changing logic

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,12 +1,25 @@
 callDBus("org.kde.plasmashell", "/PlasmaShell", "org.kde.PlasmaShell", "evaluateScript", "panels()[3].hiding = 'alwaysvisible'")
 var timer = new QTimer()
+var area
 timer.interval = 1000
 timer.singleShot = true;
-timer.timeout.connect(function() {var area = workspace.clientArea(KWin.MaximizeArea, workspace.activeScreen, workspace.currentDesktop); callDBus("org.kde.plasmashell", "/PlasmaShell", "org.kde.PlasmaShell", "evaluateScript", "panels()[3].hiding = 'windowsgobelow'")})
+timer.timeout.connect(function() {getArea()})
 timer.start()
 
+function getArea() {
+    area = workspace.clientArea(KWin.MaximizeArea, workspace.activeScreen, workspace.currentDesktop)
+    callDBus("org.kde.plasmashell", "/PlasmaShell", "org.kde.PlasmaShell", "evaluateScript", "panels()[3].hiding = 'windowsgobelow'")
+    print(area)
+    print(workspace.activeWindow)
+    workspace.windowActivated.connect(disconnectWindow)
+    workspace.activeWindow.moveResizedChanged.connect(checkWindow)
+}
+
 function checkWindow() {
-    var window = workspace.activeWindow
+    if (area == null) {
+        return
+    }
+    window = workspace.activeWindow
     print(area.y)
     print(window.frameGeometry.y)
     print(window.frameGeometry.y < area.y)
@@ -18,4 +31,8 @@ function checkWindow() {
     }
 }
 
-workspace.activeWindow.moveResizedChanged.connect(checkWindow)
+function disconnectWindow() {
+    workspace.activeWindow.moveResizedChanged.disconnect(checkWindow)
+    workspace.activeWindow.moveResizedChanged.connect(checkWindow)
+    checkWindow()
+}
